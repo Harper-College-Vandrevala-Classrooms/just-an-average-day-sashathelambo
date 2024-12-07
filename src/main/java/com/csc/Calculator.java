4bdead7a063e79598ffeb9b61a24b661732ee0b6
sashathelambo <166897058+sashathelambo@users.noreply.github.com>
2024-12-07 12:31:26 -0600

    up

--- src/main/java/com/csc/Calculator.java
+++ src/main/java/com/csc/Calculator.java
@@ -1,7 +1,64 @@
 package com.csc;
 
 public class Calculator {
+
+  public int maximumUsingForLoop(int[] nums) {
+    int max = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] > max) {
+        max = nums[i];
+      }
+    }
+    return max;
+  }
+
+  public int minimumUsingForLoop(int[] nums) {
+    int min = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] < min) {
+        min = nums[i];
+      }
+    }
+    return min;
+  }
+
+  public int sumUsingForLoop(int[] nums) {
+    int sum = 0;
+    for (int num : nums) {
+      sum += num;
+    }
+    return sum;
+  }
+
+  public double averageUsingForLoop(int[] nums) {
+    return (double) sumUsingForLoop(nums) / nums.length;
+  }
+
+  public int maximumUsingStream(int[] nums) {
+    return java.util.Arrays.stream(nums)
+                          .max()
+                          .getAsInt();
+  }
+
+  public int minimumUsingStream(int[] nums) {
+    return java.util.Arrays.stream(nums)
+                          .min()
+                          .getAsInt();
+  }
+
+  public int sumUsingStream(int[] nums) {
+    return java.util.Arrays.stream(nums)
+                          .sum();
+  }
+
+  public double averageUsingStream(int[] nums) {
+    return java.util.Arrays.stream(nums)
+                          .average()
+                          .getAsDouble();
+  }
+
   public static void main(String[] args) {
-    System.out.println("This class can house your driver program and / or your functions.");
+    Calculator calculator = new Calculator();
+    System.out.println(calculator.maximumUsingForLoop(new int[] { 1, 2, 3, 4, 5 }));
   }
 }
--- src/test/java/com/csc/TestCalculator.java
+++ src/test/java/com/csc/TestCalculator.java
@@ -7,15 +7,58 @@ import org.junit.jupiter.api.Test;
 
 public class TestCalculator {
 
-  Calculator calculator;
+    private Calculator calculator;
 
-  @BeforeEach
-  void setUp() {
-    calculator = new Calculator();
-  }
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
 
-  @Test
-  void itWorks() {
-    assertEquals(true, true);
-  }
+    @Test
+    void testMaximumUsingForLoop() {
+        int[] nums = {4, 2, 9, 6, 5};
+        assertEquals(9, calculator.maximumUsingForLoop(nums));
+    }
+
+    @Test
+    void testMinimumUsingForLoop() {
+        int[] nums = {4, 2, 9, 6, 5};
+        assertEquals(2, calculator.minimumUsingForLoop(nums));
+    }
+
+    @Test
+    void testSumUsingForLoop() {
+        int[] nums = {4, 2, 9, 6, 5};
+        assertEquals(26, calculator.sumUsingForLoop(nums));
+    }
+
+    @Test
+    void testAverageUsingForLoop() {
+        int[] nums = {4, 2, 9, 6, 5};
+        assertEquals(5.2, calculator.averageUsingForLoop(nums), 0.1);
+    }
+
+    @Test
+    void testMaximumUsingStream() {
+        int[] nums = {4, 2, 9, 6, 5};
+        assertEquals(9, calculator.maximumUsingStream(nums));
+    }
+
+    @Test
+    void testMinimumUsingStream() {
+        int[] nums = {4, 2, 9, 6, 5};
+        assertEquals(2, calculator.minimumUsingStream(nums));
+    }
+
+    @Test
+    void testSumUsingStream() {
+        int[] nums = {4, 2, 9, 6, 5};
+        assertEquals(26, calculator.sumUsingStream(nums));
+    }
+
+    @Test
+    void testAverageUsingStream() {
+        int[] nums = {4, 2, 9, 6, 5};
+        assertEquals(5.2, calculator.averageUsingStream(nums), 0.1);
+    }
 }
